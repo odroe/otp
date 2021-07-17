c9d2a24872f01e9f88ad9b423207989386e327c8
Seven Du <shiweidu@outlook.com>
2021-07-17 21:39:10 +0800

    Add dartdoc comments

--- dart/lib/src/hotp.dart
+++ dart/lib/src/hotp.dart
@@ -2,15 +2,42 @@ import 'dart:math';
 
 import 'package:crypto/crypto.dart';
 
+/// The HOTP algorithm default parameters, value that is SHA1.
 const String OTP_ALGORITHM = 'sha1';
 
+/// The RFC 4226 implentation of HOTP algorithm.
+/// 
+/// @see https://tools.ietf.org/html/rfc4226
+/// 
+/// Example
+/// ```dart
+/// final hotp = HOTP(secret);
+/// final otpValue = hotp.make(
+///   counter: 0,
+///   digits: 6,
+/// );
+/// ```
 class HOTP {
+
+  /// The HOTP use HMAC-SHA1 secret.
   final List<int> secret;
 
+  /// Create a HOTP instance.
+  /// 
+  /// @param `secret` The HOTP secret.
+  /// 
+  /// ```dart
+  /// final hotp = HOTP(secret);
+  /// ```
   const HOTP(this.secret);
 
+  /// Get using HMAC SHA1 algorithm for secret.
   Hmac get hmac => new Hmac(sha1, secret);
 
+  /// Make a OTP code.
+  /// 
+  /// @param `counter` The counter value.
+  /// @param `digits` The OTP code length.
   String make({
     int counter = 0,
     int digits = 6,
@@ -31,6 +58,11 @@ class HOTP {
     return str.padLeft(digits, '0');
   }
 
+  /// Chack a OTP code.
+  /// 
+  /// @param `otp` The OTP code.
+  /// @param `counter` The counter value.
+  /// @param `digits` The OTP code length.
   bool check(
     String otp, {
     int counter = 0,
@@ -48,6 +80,7 @@ class HOTP {
     return false;
   }
 
+  /// A `int` input to 8 length `List<int>`.
   List<int> _int_to_8_bytes(int input) {
     final bit = 8;
     return List<int>.generate(bit, (index) => (input >> (bit * index)) & 0xff)
--- dart/lib/src/totp.dart
+++ dart/lib/src/totp.dart
@@ -1,16 +1,55 @@
 import 'hotp.dart';
 
+/// The RFC 6238 algorithm implementation.
+/// 
+/// @see https://tools.ietf.org/html/rfc6238
+///
+/// Example 1:
+/// ```dart
+/// final hoto = HOTP(secret);
+/// final totp = TOTP(
+///    hoto,
+///    digits: 6,
+///    period: 30,
+/// );
+/// final value = totp.make();
+/// ```
+/// Example 2:
+/// ```dart
+/// final totp = TOTP.secret(
+///   secret,
+///   digits: 6,
+///   period: 30,
+/// );
+/// final value = totp.make();
+/// ```
 class TOTP {
+
+  /// The REC 4226 algorithm implementation.
   final HOTP hotp;
+
+  /// digits - The number of digits in the code.
   final int digits;
+
+  /// period - The number of seconds in the time period.
   final int period;
 
+  /// Create a new TOTP instance.
+  /// 
+  /// @param `hotp` - The HOTP instance.
+  /// @param `digits` - The number of digits in the code.
+  /// @param `period` - The number of seconds in the time period.
   const TOTP(
     this.hotp, {
     this.digits = 6,
     this.period = 30,
   });
 
+  /// Create a new TOTP instance for a given secret.
+  /// 
+  /// @param `secret` - The secret.
+  /// @param `digits` - The number of digits in the code.
+  /// @param `period` - The number of seconds in the time period.
   factory TOTP.secret(
     List<int> secret, {
     int digits = 6,
@@ -23,6 +62,7 @@ class TOTP {
     );
   }
 
+  /// Using current time create a counter value.
   int _create_counter() {
     final now = DateTime.now();
     final value = now.millisecondsSinceEpoch / 1000 / period;
@@ -30,11 +70,16 @@ class TOTP {
     return value.floor();
   }
 
+  /// Make a OTP value.
   String make() => hotp.make(
         counter: _create_counter(),
         digits: digits,
       );
 
+  /// Chack a OTP value.
+  /// 
+  /// @param `otp` - The OTP value.
+  /// @param `breadth` - The number of OTP values to check.
   bool check(String otp, [int breadth = 0]) => hotp.check(
         otp,
         counter: _create_counter(),
