35cfe494158bdd89bfa9d85ede9e4346b0be563e
Leonardo Razovic <leorazzo@gmail.com>
2021-07-18 15:59:15 +0200

    Add the rust_example as test

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -11,3 +11,35 @@ pub mod constants;
 pub mod hotp;
 /// TOTP is a HOTP-based one-time password algorithm, with a time value as moving factor.
 pub mod totp;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn rust_example_test() {
+        use base32;
+
+        use crate::constants::*;
+        use crate::totp::{CreateOption, Totp};
+
+        let encoded_secret = "MU2TSNRZG5TGKMBYGAZDCMJTMM3GIMJVMZRTINDFGI3WGZRVMQ4Q"; // The secret key is a base32 encoded string
+        let secret_vec =
+            base32::decode(base32::Alphabet::RFC4648 { padding: false }, encoded_secret).unwrap();
+        let secret = String::from_utf8(secret_vec).unwrap();
+        let totp = Totp::secret(&secret, CreateOption::Default);
+
+        let otp_auth_uri = "otpauth://totp/{issuer}:{account}?secret={secret}&issuer={issuer}&period={period}&digits={digits}"
+            .replace("{issuer}", "OOTP")
+            .replace("{account}", "Tester")
+            .replace("{secret}", encoded_secret)
+            .replace("{period}", &format!("{}", DEFAULT_PERIOD))
+            .replace("{digits}", &format!("{}", DEFAULT_DIGITS));
+
+        let top = "790749";
+
+        println!("valid: {:?}", totp.check(top, None));
+        let otp = totp.make();
+        println!("otp: {:?}", otp);
+        println!("uri: {:?}", otp_auth_uri);
+        assert!(totp.check(&otp, None))
+    }
+}
