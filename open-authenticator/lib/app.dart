5bfb7331c71ed121580c33e339ded5e4b99bdbdd
Seven Du <shiweidu@outlook.com>
2021-07-19 22:58:04 +0800

    Add open authenticator indicator

--- open-authenticator/lib/app.dart
+++ open-authenticator/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class Application extends StatelessWidget {
   @override
@@ -73,64 +74,20 @@ class HomeScreen extends StatelessWidget {
                     ],
                   ),
                 ),
-                CupertinoButton(
-                  child: Icon(
-                    CupertinoIcons.right_chevron,
-                    size: 16.0,
-                    color: CupertinoColors.systemGrey,
-                  ),
-                  padding: EdgeInsets.zero,
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
-            padding: EdgeInsets.symmetric(
-              horizontal: 12.0,
-              vertical: 8.0,
-            ),
-            decoration: BoxDecoration(
-              color: CupertinoDynamicColor.withBrightness(
-                color: CupertinoColors.white,
-                darkColor: CupertinoColors.white.withAlpha(30),
-              ).resolveFrom(context),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "GitHub",
-                        style: CupertinoTheme.of(context)
-                            .textTheme
-                            .navTitleTextStyle,
-                      ),
-                      Text(
-                        "medz",
-                        style: CupertinoTheme.of(context)
-                            .textTheme
-                            .textStyle
-                            .copyWith(
-                              color: CupertinoColors.systemGrey,
+                CircularPercentIndicator(
+                  radius: 26.0,
+                  lineWidth: 3.0,
+                  backgroundWidth: 1,
+                  animation: true,
+                  percent: 0.4,
+                  progressColor: CupertinoTheme.of(context).primaryColor,
+                  center: Text(
+                    '30',
+                    style:
+                        CupertinoTheme.of(context).textTheme.textStyle.copyWith(
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.w200,
                             ),
-                      ),
-                      Text(
-                        "123 856",
-                        style: CupertinoTheme.of(context)
-                            .textTheme
-                            .textStyle
-                            .copyWith(
-                              color: CupertinoTheme.of(context).primaryColor,
-                              fontSize: 36.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                    ],
                   ),
                 ),
                 CupertinoButton(
