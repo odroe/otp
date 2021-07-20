dd69b1ea78f6846ef3057ec02cb9cada46f050ed
Seven Du <shiweidu@outlook.com>
2021-07-20 17:07:59 +0800

    Open Authenticator - Using OOTP build account code

--- open-authenticator/lib/home/home.screen.dart
+++ open-authenticator/lib/home/home.screen.dart
@@ -1,64 +1,70 @@
 import 'package:flutter/cupertino.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import '../entities/origin.entity.dart';
+import '../entities/account.entity.dart';
+import 'widgets/account.card.dart';
 import 'widgets/help.dart';
-import 'widgets/origin.card.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  late Box<OriginEntity> entityBox;
-
-  @override
-  void initState() {
-    entityBox = Hive.box<OriginEntity>(OriginEntity.entityName);
-    super.initState();
-  }
+class HomeScreen extends StatelessWidget {
+  final Box<AccountEntity> accountsBox = Hive.box(AccountEntity.entityName);
 
   @override
   Widget build(BuildContext context) {
-    // CupertinoPageRoute();
-    return CupertinoPageScaffold(
-      child: CustomScrollView(
-        slivers: [
-          CupertinoSliverNavigationBar(
-            automaticallyImplyTitle: false,
-            backgroundColor: CupertinoTheme.of(context).scaffoldBackgroundColor,
-            middle: Text("Authenticator"),
-            trailing: CupertinoButton(
-              padding: EdgeInsets.zero,
-              child: Icon(CupertinoIcons.add),
-              onPressed: () {},
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: CupertinoPageScaffold(
+        child: CustomScrollView(
+          slivers: [
+            CupertinoSliverNavigationBar(
+              automaticallyImplyTitle: true,
+              automaticallyImplyLeading: true,
+              backgroundColor:
+                  CupertinoTheme.of(context).scaffoldBackgroundColor,
+              middle: Text("Authenticator"),
+              trailing: CupertinoButton(
+                padding: EdgeInsets.zero,
+                child: Icon(CupertinoIcons.add),
+                onPressed: () {
+                  // TODO: Add a code.
+                },
+              ),
+              // _kNavBarEdgePadding
+              padding: EdgeInsetsDirectional.only(
+                start: 16.0,
+                end: 16.0,
+              ),
+              largeTitle: Padding(
+                // See _kNavBarEdgePadding
+                padding: EdgeInsets.only(right: 16.0),
+                child: CupertinoSearchTextField(),
+              ),
             ),
-            largeTitle: CupertinoSearchTextField(),
-          ),
-          SliverPadding(padding: EdgeInsets.only(top: 12.0)),
-          SliverToBoxAdapter(
-            child: ValueListenableBuilder(
-              valueListenable: entityBox.listenable(),
-              builder: (BuildContext context, Box<OriginEntity> box, _widget) {
-                return ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  padding: EdgeInsets.zero,
-                  shrinkWrap: true,
-                  itemCount: box.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final entity = box.values.elementAt(index);
-                    return OriginCard();
-                  },
-                );
-              },
+            SliverPadding(padding: EdgeInsets.only(top: 12.0)),
+            SliverToBoxAdapter(
+              child: ValueListenableBuilder(
+                valueListenable: accountsBox.listenable(),
+                builder:
+                    (BuildContext context, Box<AccountEntity> box, _widget) {
+                  return ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    itemCount: box.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return AccountCard(box.values.elementAt(index));
+                    },
+                  );
+                },
+              ),
             ),
-          ),
-          SliverPadding(
-            padding: EdgeInsets.symmetric(vertical: 12.0),
-            sliver: SliverToBoxAdapter(child: Help()),
-          ),
-        ],
+            SliverPadding(
+              padding: EdgeInsets.symmetric(vertical: 12.0),
+              sliver: SliverToBoxAdapter(child: Help()),
+            ),
+          ],
+        ),
       ),
     );
   }
--- open-authenticator/lib/main.dart
+++ open-authenticator/lib/main.dart
@@ -2,16 +2,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app.dart';
-import 'entities/origin.entity.dart';
+import 'entities/account.entity.dart';
 
 Future<void> initHive() async {
   // Register adapter.
-  Hive.registerAdapter(OriginEntityAdapter());
-  Hive.registerAdapter(OriginTypeAdapter());
+  Hive.registerAdapter(AccountEntityAdapter());
+  Hive.registerAdapter(AccountTypeAdapter());
 
   // Init and open box
   await Hive.initFlutter();
-  await Hive.openBox<OriginEntity>(OriginEntity.entityName);
+  await Hive.openBox<AccountEntity>(AccountEntity.entityName);
+
+  // final account = AccountEntity();
+  // account.digits = 6;
+  // account.secret = 'MU2TSNRZG5TGKMBYGAZDCMJTMM3GIMJVMZRTINDFGI3WGZRVMQ4Q';
+  // account.period = 30;
+  // account.type = AccountType.TOTP;
+  // account.issuer = "GitHub";
+  // account.name = "medz";
+  // // account.
+  // print(box.length);
+  // // box.add(account);
 }
 
 Future<void> main() async {
