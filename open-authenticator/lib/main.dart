d7467db18cb747bfeb4cbc6b97179a5abf0f3c95
Seven Du <shiweidu@outlook.com>
2021-07-20 13:28:47 +0800

    Open Authenticator - init hive database box

--- open-authenticator/lib/main.dart
+++ open-authenticator/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox("Open Authenticator");
   runApp(Application());
 }
