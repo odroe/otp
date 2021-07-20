8fa8b013f9775d6a8241d1d647418e72b7667fc5
Seven Du <shiweidu@outlook.com>
2021-07-20 13:43:37 +0800

    Open Authenticator - Init hive and register adapter

--- open-authenticator/lib/main.dart
+++ open-authenticator/lib/main.dart
@@ -3,9 +3,19 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app.dart';
+import 'entities/origin.entity.dart';
 
-void main() async {
+Future<void> initHive() async {
+  // Register adapter.
+  Hive.registerAdapter(OriginEntityAdapter());
+  Hive.registerAdapter(OriginTypeAdapter());
+
+  // Init and open box
   await Hive.initFlutter();
-  await Hive.openBox("Open Authenticator");
+  await Hive.openBox<OriginEntity>(OriginEntity.entityName);
+}
+
+Future<void> main() async {
+  await initHive();
   runApp(Application());
 }
