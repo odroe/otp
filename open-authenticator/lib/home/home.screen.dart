5427582e04185fd0630eeb119fe6ac1c89d2a8e8
Seven Du <shiweidu@outlook.com>
2021-07-20 15:25:40 +0800

    Open Authenticator - Fetch origin's to hive box

--- open-authenticator/lib/home/home.screen.dart
+++ open-authenticator/lib/home/home.screen.dart
@@ -1,86 +1,64 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+import '../entities/origin.entity.dart';
 import 'widgets/help.dart';
-import 'widgets/progress-timer.indicator.dart';
+import 'widgets/origin.card.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Box<OriginEntity> entityBox;
+
+  @override
+  void initState() {
+    entityBox = Hive.box<OriginEntity>(OriginEntity.entityName);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // CupertinoPageRoute();
     return CupertinoPageScaffold(
-      child: ListView(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 12.0, vertical: 6.0),
-            padding: EdgeInsets.symmetric(
-              horizontal: 12.0,
-              vertical: 8.0,
+      child: CustomScrollView(
+        slivers: [
+          CupertinoSliverNavigationBar(
+            automaticallyImplyTitle: false,
+            backgroundColor: CupertinoTheme.of(context).scaffoldBackgroundColor,
+            middle: Text("Authenticator"),
+            trailing: CupertinoButton(
+              padding: EdgeInsets.zero,
+              child: Icon(CupertinoIcons.add),
+              onPressed: () {},
             ),
-            decoration: BoxDecoration(
-              color: CupertinoDynamicColor.withBrightness(
-                color: CupertinoColors.white,
-                darkColor: CupertinoColors.white.withAlpha(30),
-              ).resolveFrom(context),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "GitHub",
-                        style: CupertinoTheme.of(context)
-                            .textTheme
-                            .navTitleTextStyle,
-                      ),
-                      Text(
-                        "medz",
-                        style: CupertinoTheme.of(context)
-                            .textTheme
-                            .textStyle
-                            .copyWith(
-                              color: CupertinoColors.systemGrey,
-                            ),
-                      ),
-                      Text(
-                        "123 856",
-                        style: CupertinoTheme.of(context)
-                            .textTheme
-                            .textStyle
-                            .copyWith(
-                              color: CupertinoTheme.of(context).primaryColor,
-                              fontSize: 36.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                    ],
-                  ),
-                ),
-                ProgressTimerIndicator(period: 30),
-                CupertinoButton(
-                  child: Icon(
-                    CupertinoIcons.right_chevron,
-                    size: 16.0,
-                    color: CupertinoColors.systemGrey,
-                  ),
+            largeTitle: CupertinoSearchTextField(),
+          ),
+          SliverPadding(padding: EdgeInsets.only(top: 12.0)),
+          SliverToBoxAdapter(
+            child: ValueListenableBuilder(
+              valueListenable: entityBox.listenable(),
+              builder: (BuildContext context, Box<OriginEntity> box, _widget) {
+                return ListView.builder(
+                  physics: const BouncingScrollPhysics(),
                   padding: EdgeInsets.zero,
-                  onPressed: () {},
-                ),
-              ],
+                  shrinkWrap: true,
+                  itemCount: box.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final entity = box.values.elementAt(index);
+                    return OriginCard();
+                  },
+                );
+              },
             ),
           ),
-          const Help(),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(vertical: 12.0),
+            sliver: SliverToBoxAdapter(child: Help()),
+          ),
         ],
-      ),
-      navigationBar: CupertinoNavigationBar(
-        backgroundColor: CupertinoTheme.of(context).scaffoldBackgroundColor,
-        middle: CupertinoSearchTextField(),
-        trailing: CupertinoButton(
-          padding: EdgeInsets.zero,
-          child: Icon(CupertinoIcons.add),
-          onPressed: () {},
-        ),
       ),
     );
   }
